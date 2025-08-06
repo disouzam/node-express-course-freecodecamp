b2a28594fa73ddc15b0e380c8a2e6c338ed38b86
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-08-05 22:43:01 -0300

    Example taken from Geeks for Geeks website teaching how to stop a setInterval execution using a counter

--- 01-node-tutorial/globals-example.js
+++ 01-node-tutorial/globals-example.js
@@ -1,3 +1,14 @@
+// https://www.geeksforgeeks.org/javascript/how-to-stop-setinterval-call-in-javascript/
+let counter = 0;
+let intervalId = setInterval(() => {
+    counter++;
+    console.log('Counter:', counter);
+    if (counter >= 5) {
+        clearInterval(intervalId);
+        console.log('Interval stopped after 5 iterations.');
+    }
+}, 1000);
+
 // GLOBALS  - NO WINDOW !!!!
 
 // __dirname  - path to current directory
@@ -12,8 +23,33 @@ console.log(`require: ${require}`);
 console.log(`module: ${module}`);
 console.log(`process: ${process}`);
 
-setInterval(() => {
-  console.log("Hello World");
+// Further information about globals: https://nodejs.org/api/globals.html
+
+const ac = new AbortController();
+
+ac.signal.addEventListener('abort', () => console.log('Aborted!'),
+                           { once: true });
+
+ac.abort();
+
+console.log(ac.signal.aborted);  // Prints true
+
+let counter2 = 0;
+let intervalId2 = setInterval(() => {
+    counter2++;
+    console.log('Counter2:', counter2);
+    if (counter2 >= 15) {
+        clearInterval(intervalId2);
+        console.log('Interval 2 stopped after 15 iterations.');
+    }
 }, 1000);
 
-// Further information about globals: https://nodejs.org/api/globals.html
+let counter3 = 0;
+let intervalId3 = setInterval(() => {
+    counter3++;
+    console.log('Counter3:', counter3);
+    if (counter3 >= 25) {
+        clearInterval(intervalId3);
+        console.log('Interval 3 stopped after 25 iterations.');
+    }
+}, 1000);
